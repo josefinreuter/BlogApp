156cc93e033de9ad67971e2795971919a1bd5965
Josefin Reuter <josefin@josefinreuter.fi>
2018-09-18 19:46:54 +0300

    activating tests

--- blogilista/tests/list_helper.test.js
+++ blogilista/tests/list_helper.test.js
@@ -2,7 +2,7 @@ const listHelper = require('../utils/list_helper')
 
 const {emptyList, listWithOneBlog, blogs} = require('./tests_helper')
 
-describe.skip('list helpers', () => {
+describe('list helpers', () => {
     test('dummy is called', () => {
         const blogs = []
 
